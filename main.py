e87fd4457d8dc4564c6941e3b3c6e4c5787140c5
Florian Amary <florianamary@gmail.com>
2023-05-04 17:44:12 +0200

    fintion exo 2

--- main.py
+++ main.py
@@ -30,72 +30,88 @@ with open("pi_en_fichier.txt","w",encoding="utf-8") as f:
 import csv
 
 class Etudiant:
-    def __init__(self,nom,annee_naissance,gpa,connais_python):
-        self.nom=nom
-        self.annee_naissance=annee_naissance
-        self.gpa=gpa
-        self.connais_python=connais_python
+    def __init__(self, nom:str, annee_naissance:int, gpa:float, connais_python:bool):
+        self.nom = nom
+        self.annee_naissance = annee_naissance
+        self.gpa = gpa
+        self.connais_python = connais_python
 
     @property
     def nom(self):
-        return self.__nom
+        return self._nom
     @nom.setter
-    def nom(self, nom:str):
+    def nom(self, nom):
         if not isinstance(nom, str):
-            raise TypeError("Entrez une chaîne de caractère")
-        self.__nom = nom
-
+            raise TypeError("Entrer un str")
+        self._nom = nom
     @property
     def annee_naissance(self):
-        return self.__annee_naissance
+        return self._annee_naissance
     @annee_naissance.setter
     def annee_naissance(self, annee_naissance):
         if not isinstance(annee_naissance, int):
-            raise TypeError("Entrez un int")
-        self.__annee_naissance=annee_naissance
-
+            raise TypeError("Entrer un int")
+        self._annee_naissance = annee_naissance
     @property
     def gpa(self):
-        return self.__gpa
+        return self._gpa
     @gpa.setter
     def gpa(self, gpa):
         if not isinstance(gpa, float):
-            raise TypeError("Entrez un float")
-        self.__gpa=gpa
-
+            raise TypeError("Entrer un float")
+        if gpa < 0:
+            raise ValueError("Entrer une valeur positive")
+        self._gpa = gpa
     @property
     def connais_python(self):
-        return self.__connais_python
+        return self._connais_python
     @connais_python.setter
     def connais_python(self, connais_python):
         if not isinstance(connais_python, bool):
-            raise TypeError("Entrez un booléen")
-        self.__connais_python=connais_python
+            raise TypeError("Entrer un bolléen")
+        self._connais_python = connais_python
 
     def to_dict(self):
-         print( {"nom": self.nom , "annee_naissance":self.annee_naissance,"gpa":self.gpa,"connais_python":self.connais_python})
+        return {"Nom" : self.nom, "Annee de naissance" : self.annee_naissance, "GPA" : self.gpa, "Connais PYTHON" : self.connais_python}
 
-    @staticmethod
     def from_dict(d):
-        return Etudiant(
-            nom=d["nom"],
-            annee_naissance=int(d["annee_naissance"]),
-            gpa=float(d["gpa"]),
-            connais_python=bool(d["connais_python"])
-        )
+        return Etudiant(nom=d.get("Nom"), annee_naissance=d.get("Annee de naissance"), gpa=d.get("GPA"), connais_python=d.get("Connais PYTHON"))
 
+class Groupe(Etudiant):
+    def __init__(self):
+        self.liste_etudiants = list()
 
+    def ajouter_etudiant(self, object):
+        self.liste_etudiants.append(object)
 
-class Groupe:
-    def __init__(self,etudiants):
-        self.etudiants=etudiants
-    def sauvegarder_csv(self,chemin):
-        with open('chemin', 'w', newline='') as f:
-            fieldnames = ['nom', 'annee_naissance','gpa','connais_python']
-            writer = f.DictWriter(f, fieldnames=fieldnames)
+    def sauvegarder_csv(self, chemin):
+        with open(chemin, "w", newline="") as file:
+            writer = csv.DictWriter(file, ["Nom", "Annee de naissance", "GPA", "Connais PYTHON"])
             writer.writeheader()
-            for etudiant in self.etudiants:
-                writer.writerow(etudiant.to_dict)
+            for etudiant in self.liste_etudiants:
+                writer.writerow(etudiant.to_dict())
+
+    def charger_csv(chemin):
+        with open(chemin, "r") as file:
+            content = file.read().split("\n")
+            content = content[1:-1]
+            obj_temporaire = Groupe()
+            for etudiants in content:
+                infos = etudiants.split(",")
+                obj_temporaire.ajouter_etudiant(Etudiant(nom=infos[0], annee_naissance=int(infos[1]), gpa=float(infos[2]), connais_python=bool(infos[3])))
+        return obj_temporaire
+
+
+def Ex2():
+    E1 = Etudiant(nom="Pestrimaux", annee_naissance=2002, gpa=1.0, connais_python=True)
+    E2 = Etudiant(nom="Barrios", annee_naissance=2001, gpa=1.1, connais_python=False)
+    groupe = Groupe()
+    groupe.ajouter_etudiant(E1)
+    groupe.ajouter_etudiant(E2)
+    groupe.sauvegarder_csv("Groupe.csv")
+
+    groupe2 = Groupe.charger_csv("Groupe.csv")
+    return groupe
 
 etudiant1=Etudiant("Florian",2002,2.8,False)
 etudiant2=Etudiant("Kevin",2004,5.0,True)
@@ -107,7 +123,7 @@ etudiant2.to_dict()
 etudiant3.to_dict()
 etudiant4.to_dict()
 
-liste=Groupe([etudiant1,etudiant2,etudiant3,etudiant4])
+
 
 
 #Exercice 3
